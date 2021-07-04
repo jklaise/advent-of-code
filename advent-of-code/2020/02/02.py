33e059425c4b9c6714250c1dd240983a6aacfb5a
Janis Klaise <janis.klaise@gmail.com>
2021-07-04 23:45:21 +0100

    2020/02 Python part 2

--- advent-of-code/2020/02/02.py
+++ advent-of-code/2020/02/02.py
@@ -1,3 +1,6 @@
+from operator import eq, xor
+
+
 def read_input(filename: str) -> list:
     lows, highs, letters, passwords = [], [], [], []
     with open(filename) as f:
@@ -21,7 +24,20 @@ def count_valid(lows: list, highs: list, letters: list, passwords: list) -> int:
     return sum(map(ge_and_le, map(str.count, passwords, letters), lows, highs))
 
 
+def count_valid_new(pos1: list, pos2: list, letters: list, passwords: list) -> int:
+    return sum(
+        xor(pwd[p1 - 1] == letter, pwd[p2 - 1] == letter)
+        for p1, p2, letter, pwd in zip(pos1, pos2, letters, passwords)
+    )
+
+
 if __name__ == "__main__":
+    # part 1
     lows, highs, letters, passwords = read_input("input.txt")
     valid_passwords = count_valid(lows, highs, letters, passwords)
     print(f"Number of valid passwords: {valid_passwords}")
+
+    # part 2
+    pos1, pos2 = lows, highs
+    valid_passwords = count_valid_new(pos1, pos2, letters, passwords)
+    print(f"Number of valid passwords (part 2): {valid_passwords}")
