04b95a3565e48345e00190b33b1c38698cfdc58e
Janis Klaise <janis.klaise@gmail.com>
2021-12-07 00:41:39 +0000

    2021/06 - Part 2

--- 2021/06/main.py
+++ 2021/06/main.py
@@ -23,6 +23,17 @@ def update_state(state: List[int]) -> List[int]:
     return state
 
 
+def update_counts(counter: Counter) -> Counter:
+    new_counter = Counter()
+    for lifetime, number in counter.items():
+        if lifetime == 0:
+            new_counter[8] += number
+            new_counter[6] += number
+        else:
+            new_counter[lifetime - 1] += number
+    return new_counter
+
+
 if __name__ == '__main__':
     with open('input.txt') as f:
         data = f.read().strip().split(',')
@@ -32,3 +43,9 @@ if __name__ == '__main__':
     N_DAYS = 80
     result = reduce(compose, [data, *[update_state] * N_DAYS])
     print(f'Answer to part 1 is {len(result)}')
+
+    # Part 2
+    N_DAYS = 256
+    counter = Counter(data)
+    result = reduce(compose, [counter, *[update_counts] * N_DAYS])
+    print(f'Answer to part 2 is {sum(result.values())}')
